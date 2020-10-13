47902f6f31b87cfa8867ba26d8e105ea1b253ff3
Jose Miguel Carvajal Jimenez <josemcj07@gmail.com>
2020-10-13 00:01:39 -0500

    mal paralelizado

--- testing/reduccion.cpp
+++ testing/reduccion.cpp
@@ -2,14 +2,20 @@
 #include <opencv2/highgui.hpp>
 #include <pthread.h>
 #include <iostream>
+#include <sys/time.h>
 
 using namespace std;
 using namespace cv;
 
-Mat image;
+//Mat image;
 int num_threads;
 
 void* transform4kto480(void* arg);
+
+struct transform4kto480_struct{
+    Mat image;
+    Mat result;
+};
 
 
 int main(int argc, char** argv) {
@@ -17,46 +23,54 @@ int main(int argc, char** argv) {
     // Check the number of parameters
     if (argc < 3) {
         // Tell the user how to run the program
-        cerr << "Uso:" << argv[0] << " Imagen #hilos (Ejemplo:./reduccion 4k.jpg 8)"<< endl;
+        cerr << "Uso:" << argv[0] << " Imagen #Hilos (Ejemplo:./reduccion 4k.jpg 8)"<< endl;
         /* "Usage messages" are a conventional way of telling the user
          * how to run a program if they enter the command incorrectly.
          */
         return 1;
     }
 
-    Mat image = imread(argv[1], IMREAD_COLOR);
+    struct timeval tval_before, tval_after, tval_result;
+
+    gettimeofday(&tval_before, NULL);
+    //Mat image = imread(argv[1], IMREAD_COLOR);
     num_threads = atoi(argv[2]);
     //Trhead ID
     pthread_t tids[num_threads];
     //Create Attributes
-    
+    struct transform4kto480_struct args[num_threads];
 
-    for(int i=0; i<num_threads;i++){
+    for(int i=0; i<num_threads;i++){    
+    args[i].image = imread(argv[1], IMREAD_COLOR);    
     pthread_attr_t attr;
     pthread_attr_init(&attr);
-    pthread_create(&tids[i],&attr, transform4kto480, &image);
+    pthread_create(&tids[i],&attr, transform4kto480, &args[i]);
     }
     //Wait until thread has done its work
     for(int i=0; i<num_threads;i++){
     pthread_join(tids[i], NULL);
+    imwrite("4kto480.jpg", args[i].result);
     } 
-    //transform4kto480(argv[1]);
+    
+    gettimeofday(&tval_after, NULL);
+
+    timersub(&tval_after,&tval_before,&tval_result);
+
+    printf("Time elapsed with %d threads: %ld.%06lds\n", num_threads, (long int)tval_result.tv_sec, (long int)tval_result.tv_usec);
     return 0;
 }
 
 void* transform4kto480(void* arg){
 
-    Mat *image_ptr = (Mat*) arg;
-    Mat image = *image_ptr;
+    struct transform4kto480_struct *arg_struct = (struct transform4kto480_struct*) arg;
     
-    if(image.empty()) {
-        cout << "Error: the image has been incorrectly loaded." << endl;
+    if(arg_struct->image.empty()) {
+        cout << "Error: the arg_struct->image has been incorrectly loaded." << endl;
     }
 
-    Mat temp(image.rows + 2, image.cols + 2, CV_8UC3, Scalar(255,255, 255));
+    Mat temp(arg_struct->image.rows + 2, arg_struct->image.cols + 2, CV_8UC3, Scalar(255,255, 255));
     
-    //tamaño de 4k a 480
-    Mat copy( image.rows*2/9, image.cols/6, CV_8UC3, Scalar(255,255, 255));
+    Mat copy( arg_struct->image.rows*2/9, arg_struct->image.cols/6, CV_8UC3, Scalar(255,255, 255));
 
     Vec3b cpixel;
     cpixel[0] = (uchar) 0;
@@ -69,35 +83,35 @@ void* transform4kto480(void* arg){
     temp.at<Vec3b>(temp.rows - 1, temp.cols - 1) = cpixel;
 
 
-    for(int i = 0; i < image.rows ; i++) {
-        for(int j = 0; j < image.cols; j++) {
-            cpixel = image.at<Vec3b>(i, j);
+    for(int i = 0; i < arg_struct->image.rows ; i++) {
+        for(int j = 0; j < arg_struct->image.cols; j++) {
+            cpixel = arg_struct->image.at<Vec3b>(i, j);
             temp.at<Vec3b>(i+1, j+1) = cpixel;
         }
     }
 
-    for(int i = 0; i < image.rows; i++){
-        cpixel = image.at<Vec3b>(i, 0);
+    for(int i = 0; i < arg_struct->image.rows; i++){
+        cpixel = arg_struct->image.at<Vec3b>(i, 0);
         temp.at<Vec3b>(i+1, 0) = cpixel;
     }
 
-    for(int i = 0; i < image.rows; i++){
-        cpixel = image.at<Vec3b>(i, image.cols - 1);
+    for(int i = 0; i < arg_struct->image.rows; i++){
+        cpixel = arg_struct->image.at<Vec3b>(i, arg_struct->image.cols - 1);
         temp.at<Vec3b>(i+1, temp.cols - 1) = cpixel;
     }
 
-    for(int i = 0; i < image.cols; i++){
-        cpixel = image.at<Vec3b>(0, i);
+    for(int i = 0; i < arg_struct->image.cols; i++){
+        cpixel = arg_struct->image.at<Vec3b>(0, i);
         temp.at<Vec3b>(0, i + 1) = cpixel;
     }
 
-    for(int i = 0; i < image.cols; i++){
-        cpixel = image.at<Vec3b>(image.rows - 1, i);
+    for(int i = 0; i < arg_struct->image.cols; i++){
+        cpixel = arg_struct->image.at<Vec3b>(arg_struct->image.rows - 1, i);
         temp.at<Vec3b>(temp.rows - 1, i + 1) = cpixel;
     }
 
-    for(int i = 0; i < image.rows; i++){
-        for(int j = 0; j < image.cols; j++){
+    for(int i = 0; i < arg_struct->image.rows; i++){
+        for(int j = 0; j < arg_struct->image.cols; j++){
             Vec3b mpixel = temp.at<Vec3b>(i+1, j+1);
             Vec3b upixel = temp.at<Vec3b>(i, j+1);
             Vec3b dpixel = temp.at<Vec3b>(i+2, j+1);
@@ -122,14 +136,6 @@ void* transform4kto480(void* arg){
         }
     }
 
-    imwrite("4kto480.jpg", copy);
-/*
-    // Then we create a window to display our image
-    namedWindow("My first OpenCV window");
-
-    // Finally, we display our image and ask the program to wait for a key to be pressed
-    imshow("My first OpenCV window", copy);
-    waitKey(0);
-*/
+    arg_struct->result = copy;
     pthread_exit(0);
 }
