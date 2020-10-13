be70a39ca8cbe95da8714d7faee068a74d128576
Jose Miguel Carvajal Jimenez <josemcj07@gmail.com>
2020-10-13 18:12:17 -0500

    print into files

--- testing/reduccion1080/reduccion1080.cpp
+++ testing/reduccion1080/reduccion1080.cpp
@@ -3,6 +3,7 @@
 #include <pthread.h>
 #include <iostream>
 #include <sys/time.h>
+#include <stdio.h>
 
 using namespace std;
 using namespace cv;
@@ -56,7 +57,10 @@ int main(int argc, char** argv) {
 
     timersub(&tval_after,&tval_before,&tval_result);
 
-    printf("Time elapsed with %d threads: %ld.%06lds\n", num_threads, (long int)tval_result.tv_sec, (long int)tval_result.tv_usec);
+    FILE * pFile;
+    pFile = fopen("resultados.txt", "w");
+    fprintf(pFile, "Time elapsed with %d threads: %ld.%06lds\n", num_threads, (long int)tval_result.tv_sec, (long int)tval_result.tv_usec);
+    fclose(pFile);    
     return 0;
 }
 
