a93fc224e1595d3e7f8122b4e651a25c04643ebd
Jose Miguel Carvajal Jimenez <josemcj07@gmail.com>
2020-10-14 23:01:36 -0500

    organizing files

---
+++ Pthreads/reduccion720/CMakeFiles/reduccion720.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reduccion720.dir/reduccion720.cpp.o"
+  "reduccion720"
+  "reduccion720.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/reduccion720.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
