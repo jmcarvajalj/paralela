47a4eaacd82e933587faa5c80e03ee6a7d58b075
josemiguelcarvajal07 <josemcj07@gmail.com>
2020-11-23 12:00:03 -0500

    changing matrix.cu

--- CUDA/matrix.cu
+++ CUDA/matrix.cu
@@ -1,43 +1,91 @@
 #include <iostream>
 #include <stdio.h>
+#include <math.h>
 #include <sys/time.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-#define N (2048*2048);
-#define THREADS_PER_BLOCK 512;
-
-__global__ void add(int *a, int *b, int *c, int n);
-
-int main (void) {
-    int *a, *b, *c; //host copies of a, b and c
-    int *d_a, *d_b, *d_c; // device copies of a, b, c
-    int size = N * sizeof(int);
-    // Alloc space for device copies of a, b, c
-    cudaMalloc((void **)&d_a, size);
-    cudaMalloc((void **)&d_b, size);
-    cudaMalloc((void **)&d_c, size);
-    // Alloc space for host copies of a, b, c and setup input values
-    a = (int *)malloc(size); random_ints(a, N);
-    b = (int *)malloc(size); random_ints(b, N);
-    c = (int *)malloc(size);
-    
-    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
-    // Launch add() kernel on GPU
-    //add<<<N/THREADS_PER_BLOCK,THREADS_PER_BLOCK>>>(d_a, d_b, d_c);
-    add<<<(N + M-1) / M,M>>>(d_a, d_b, d_c, N);
-    // Copy result back to host
-    cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
-    // Cleanup
-    free(a); free(b); free(c);
-    cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+// CUDA kernel. Each thread takes care of one element of c
+__global__ void vecAdd(double *a, double *b, double *c, int n)
+{
+    // Get our global thread ID
+    int id = blockIdx.x*blockDim.x+threadIdx.x;
+ 
+    // Make sure we do not go out of bounds
+    if (id < n)
+        c[id] = a[id] + b[id];
+}
+ 
+int main( int argc, char* argv[] )
+{
+    // Size of vectors
+    int n = 100000;
+ 
+    // Host input vectors
+    double *h_a;
+    double *h_b;
+    //Host output vector
+    double *h_c;
+ 
+    // Device input vectors
+    double *d_a;
+    double *d_b;
+    //Device output vector
+    double *d_c;
+ 
+    // Size, in bytes, of each vector
+    size_t bytes = n*sizeof(double);
+ 
+    // Allocate memory for each vector on host
+    h_a = (double*)malloc(bytes);
+    h_b = (double*)malloc(bytes);
+    h_c = (double*)malloc(bytes);
+ 
+    // Allocate memory for each vector on GPU
+    cudaMalloc(&d_a, bytes);
+    cudaMalloc(&d_b, bytes);
+    cudaMalloc(&d_c, bytes);
+ 
+    int i;
+    // Initialize vectors on host
+    for( i = 0; i < n; i++ ) {
+        h_a[i] = sin(i)*sin(i);
+        h_b[i] = cos(i)*cos(i);
+    }
+ 
+    // Copy host vectors to device
+    cudaMemcpy( d_a, h_a, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy( d_b, h_b, bytes, cudaMemcpyHostToDevice);
+ 
+    int blockSize, gridSize;
+ 
+    // Number of threads in each thread block
+    blockSize = 1024;
+ 
+    // Number of thread blocks in grid
+    gridSize = (int)ceil((float)n/blockSize);
+ 
+    // Execute the kernel
+    vecAdd<<<gridSize, blockSize>>>(d_a, d_b, d_c, n);
+ 
+    // Copy array back to host
+    cudaMemcpy( h_c, d_c, bytes, cudaMemcpyDeviceToHost );
+ 
+    // Sum up vector c and print result divided by n, this should equal 1 within error
+    double sum = 0;
+    for(i=0; i<n; i++)
+        sum += h_c[i];
+    printf("final result: %f\n", sum/n);
+ 
+    // Release device memory
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+ 
+    // Release host memory
+    free(h_a);
+    free(h_b);
+    free(h_c);
+ 
     return 0;
 }
-
-__global__ void add(int *a, int *b, int *c, int n) {
-    int index = threadIdx.x + blockIdx.x * blockDim.x;
-    if (index < n)
-     c[index] = a[index] + b[index];
-}
-    
