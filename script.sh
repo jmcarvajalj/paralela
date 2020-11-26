031fc63236598a2ca147291f41f00048031623e2
josemiguelcarvajal07 <josemcj07@gmail.com>
2020-11-26 01:28:37 -0500

    cmake cuda test 13

--- script.sh
+++ script.sh
@@ -49,9 +49,7 @@ printf "CUDA\n\n" >> resultados.txt &&
 cd CUDA/ && cd reduccion4k/ && cmake . && make && ./reduccion4k 4k.jpg result.jpg 2 20 &&
 ./reduccion4k 4k.jpg result.jpg 4 20 && ./reduccion4k 4k.jpg result.jpg 8 20 && 
 ./reduccion4k 4k.jpg result.jpg 16 20 && 
-printf "\n" >> resultados.txt &&
-printf "\n" >> resultados.txt &&
-printf "\n" >> resultados.txt &&git
+printf "\n\n" >> resultados.txt &&
 ./reduccion4k 4k.jpg result.jpg 2 40 && ./reduccion4k 4k.jpg result.jpg 4 40 && 
 ./reduccion4k 4k.jpg result.jpg 8 40 && ./reduccion4k 4k.jpg result.jpg 16 40 &&
 cd .. && cd .. &&
@@ -59,9 +57,7 @@ printf "\n" >> resultados.txt &&
 cd CUDA/ && cd reduccion1080/ && cmake . && make && ./reduccion1080 1080.jpg result.jpg 2 20 &&
 ./reduccion1080 1080.jpg result.jpg 4 20 && ./reduccion1080 1080.jpg result.jpg 8 20 && 
 ./reduccion1080 1080.jpg result.jpg 16 20 && 
-printf "\n" >> resultados.txt &&
-printf "\n" >> resultados.txt &&
-printf "\n" >> resultados.txt &&
+printf "\n\n" >> resultados.txt &&
 ./reduccion1080 1080.jpg result.jpg 2 40 && ./reduccion1080 1080.jpg result.jpg 4 40 && 
 ./reduccion1080 1080.jpg result.jpg 8 40 && ./reduccion1080 1080.jpg result.jpg 16 40 &&
 cd .. && cd .. &&
@@ -69,9 +65,7 @@ printf "\n" >> resultados.txt &&
 cd CUDA/ && cd reduccion720/ && cmake . && make && ./reduccion720 720.jpg result.jpg 2 20 &&
 ./reduccion720 720.jpg result.jpg 4 20 && ./reduccion720 720.jpg result.jpg 8 20 && 
 ./reduccion720 720.jpg result.jpg 16 20 && 
-printf "\n" >> resultados.txt &&
-printf "\n" >> resultados.txt &&
-printf "\n" >> resultados.txt &&
+printf "\n\n" >> resultados.txt &&
 ./reduccion720 720.jpg result.jpg 2 40 && ./reduccion720 720.jpg result.jpg 4 40 && 
 ./reduccion720 720.jpg result.jpg 8 40 && ./reduccion720 720.jpg result.jpg 16 40 &&
 cd .. && cd .. &&
