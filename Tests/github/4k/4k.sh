4630bc90b8deb2c3694fdd8a2b816b7d4224bcef
josemiguelcarvajal07 <josemcj07@gmail.com>
2020-11-25 23:26:05 -0500

    probando reduccion4k en colab

---
+++ Tests/github/4k/4k.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp /content/paralela/Tests/github/4k/result.jpg /content/paralela/CUDA/reduccion4k/result.jpg
+
--- test/sad/f.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "SUCCESS!\n";
-    return 0;
-}
